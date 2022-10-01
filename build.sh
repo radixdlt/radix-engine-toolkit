6c984dfa97c5e66f09607a745ecd330d038eae0c
0xOmarA <OmarAbdulla7@hotmail.com>
2022-10-01 15:25:20 +0300

    header generation and XC Framework

--- build.sh
+++ build.sh
@@ -24,4 +24,40 @@ cargo build --target aarch64-apple-ios --release        # iOS iPhone Aarch64 tar
         -o iOS/iPhone/libtransaction_library.a
 )
 
+# Create the C header of the provided functions and adding it to the directory of each of the 
+# builds
+(
+    rustup default nightly
+
+    # Creating the header file
+    cbindgen \
+        --config cbindgen.toml \
+        --crate transaction-library \
+        --output transaction_library.h
+
+    # Copying the header file to all of the builds
+    cp transaction_library.h ./target/wasm32-unknown-unknown/release/
+    cp transaction_library.h ./target/x86_64-apple-ios/release/
+    cp transaction_library.h ./target/aarch64-apple-ios-sim/release/
+    cp transaction_library.h ./target/aarch64-apple-ios/release/
+    
+    cp transaction_library.h ./target/iOS/iPhone
+    cp transaction_library.h ./target/iOS/simulator
+    
+    # The root-level header is no longer needed now. Safe to delete
+    rm transaction_library.h
+
+    rustup default stable
+)
+
+# Creating an XC Framework of the static libraries. Note: at the current moment of time, I have
+# removed the `x86_64-apple-ios` target from the XC Framework as it clashed with 
+# `ios-arm64-simulator`. If this causes issues, then we can look into it further. 
+xcodebuild -create-xcframework \
+    -library ./target/aarch64-apple-ios/release/libtransaction_library.a \
+    -headers ./target/aarch64-apple-ios/release/transaction_library.h \
+    -library ./target/aarch64-apple-ios-sim/release/libtransaction_library.a \
+    -headers ./target/aarch64-apple-ios-sim/release/transaction_library.h \
+    -output ./target/iOS/universal.xcframework
+
 echo "Done!"
