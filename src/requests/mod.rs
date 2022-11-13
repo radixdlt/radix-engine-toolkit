e460af20daeb8553172a8308488a57e32f97704c
0xOmarA <OmarAbdulla7@hotmail.com>
2022-11-13 18:25:15 +0300

    Added support for virtual account address derivations

--- src/requests/mod.rs
+++ src/requests/mod.rs
@@ -19,10 +19,9 @@ pub mod encode_address_request;
 pub mod sbor_decode_request;
 pub mod sbor_encode_request;
 
-pub mod extract_abi_request;
-
 pub mod derive_non_fungible_address_from_public_key_request;
 pub mod derive_non_fungible_address_request;
+pub mod derive_virtual_account_address;
 
 pub use compile_notarized_transaction_intent_request::*;
 pub use compile_signed_transaction_intent_request::*;
@@ -35,8 +34,8 @@ pub use decompile_transaction_intent_request::*;
 pub use decompile_unknown_transaction_intent_request::*;
 pub use derive_non_fungible_address_from_public_key_request::*;
 pub use derive_non_fungible_address_request::*;
+pub use derive_virtual_account_address::*;
 pub use encode_address_request::*;
-pub use extract_abi_request::*;
 pub use information_request::*;
 pub use sbor_decode_request::*;
 pub use sbor_encode_request::*;
