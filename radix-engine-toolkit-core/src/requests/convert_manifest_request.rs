d333ceb06d864cf4b39a104bbc36a3ece875ca6f
0xOmarA <OmarAbdulla7@hotmail.com>
2022-12-05 13:54:44 +0300

    License

--- radix-engine-toolkit-core/src/requests/convert_manifest_request.rs
+++ radix-engine-toolkit-core/src/requests/convert_manifest_request.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 //! Defines the request and response models for the convert manifest request. This request is made
 //! when the client has a manifest in one format (JSON as an example) and they wish to convert
 //! the manifest to another format (String as an example). The conversion between the supported
