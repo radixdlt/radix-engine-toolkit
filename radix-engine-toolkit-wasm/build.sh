20e843b2247c98c6468c98233da7fe9dc8fc678c
Marek Karwacki <marek.karwacki@rdx.works>
2022-12-06 11:50:48 +0100

    Test CI

--- radix-engine-toolkit-wasm/build.sh
+++ radix-engine-toolkit-wasm/build.sh
@@ -22,12 +22,12 @@ cargo \
     --target wasm32-unknown-unknown \
     --release
 
-echo "Using wasm-opt to compress the WASM"
+#echo "Using wasm-opt to compress the WASM"
 
-wasm-opt \
-    -Oz -g \
-    --strip-debug --strip-dwarf --strip-producers \
-    -o ./target/wasm32-unknown-unknown/release/radix_engine_toolkit_wasm.wasm \
-    ./target/wasm32-unknown-unknown/release/radix_engine_toolkit_wasm.wasm
+#wasm-opt \
+#    -Oz -g \
+#    --strip-debug --strip-dwarf --strip-producers \
+#    -o ./target/wasm32-unknown-unknown/release/radix_engine_toolkit_wasm.wasm \
+#    ./target/wasm32-unknown-unknown/release/radix_engine_toolkit_wasm.wasm
 
-echo "WASM build completed"
+#echo "WASM build completed"
