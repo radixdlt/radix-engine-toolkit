41a89ae5593b357c0bbe46192882badcffca9400
Omar <OmarAbdulla7@hotmail.com>
2023-03-04 19:57:43 +0300

    Schema generation for analyze manifest with preview

--- schema/src/main.rs
+++ schema/src/main.rs
@@ -63,6 +63,8 @@ pub fn generate_json_schema() -> Result<(), GenerationError> {
         ConvertManifestResponse,
         AnalyzeManifestRequest,
         AnalyzeManifestResponse,
+        AnalyzeManifestWithPreviewContextRequest,
+        AnalyzeManifestWithPreviewContextResponse,
         CompileTransactionIntentRequest,
         CompileTransactionIntentResponse,
         DecompileTransactionIntentRequest,
