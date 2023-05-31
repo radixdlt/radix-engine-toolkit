883535779059393dc2a3d3c2517d7964e00e3e86
Omar <OmarAbdulla7@hotmail.com>
2023-05-31 14:55:49 +0300

    clean slate

--- benches/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=dylib=radix_engine_toolkit");
-    println!("cargo:rustc-link-search=native=./lib");
-}
--- format.sh
+++ format.sh
@@ -1,3 +1,1 @@
 cargo +nightly fmt
-(cd native-json-interface; cargo +nightly fmt)
-(cd cli-json-interface; cargo +nightly fmt)
