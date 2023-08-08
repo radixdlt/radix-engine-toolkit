c92947484052a5dbdfb6428bb87a2dad798ff87a
Omar <OmarAbdulla7@hotmail.com>
2023-08-08 04:30:13 +0300

    Uniffi: clean up the KnownAddresses struct definition and new fn

--- radix-engine-toolkit-uniffi/src/utils/functions.rs
+++ radix-engine-toolkit-uniffi/src/utils/functions.rs
@@ -19,11 +19,7 @@ use crate::prelude::*;
 
 #[uniffi::export]
 pub fn known_addresses(network_id: u8) -> KnownAddresses {
-    KnownAddresses {
-        resource_addresses: ResourceAddresses::new_from_network(network_id),
-        package_addresses: PackageAddresses::new_from_network(network_id),
-        component_addresses: ComponentAddresses::new_from_network(network_id),
-    }
+    KnownAddresses::new_from_network(network_id)
 }
 
 #[uniffi::export]
@@ -31,84 +27,86 @@ pub fn hash(data: Vec<u8>) -> Arc<Hash> {
     Hash::from_unhashed_bytes(data)
 }
 
-#[derive(Clone, Debug, Record)]
-pub struct KnownAddresses {
-    pub resource_addresses: ResourceAddresses,
-    pub package_addresses: PackageAddresses,
-    pub component_addresses: ComponentAddresses,
-}
-
-impl_new_from_network! {
-    #[derive(Clone, Debug, Record)]
-    pub struct ResourceAddresses {
-        pub xrd: Arc<Address>,
-        pub secp256k1_signature_virtual_badge: Arc<Address>,
-        pub ed25519_signature_virtual_badge: Arc<Address>,
-        pub package_of_direct_caller_virtual_badge: Arc<Address>,
-        pub global_caller_virtual_badge: Arc<Address>,
-        pub system_transaction_badge: Arc<Address>,
-        pub package_owner_badge: Arc<Address>,
-        pub validator_owner_badge: Arc<Address>,
-        pub account_owner_badge: Arc<Address>,
-        pub identity_owner_badge: Arc<Address>,
-    }
-}
-
-impl_new_from_network! {
-    #[derive(Clone, Debug, Record)]
-    pub struct PackageAddresses {
-        pub package_package: Arc<Address>,
-        pub resource_package: Arc<Address>,
-        pub account_package: Arc<Address>,
-        pub identity_package: Arc<Address>,
-        pub consensus_manager_package: Arc<Address>,
-        pub access_controller_package: Arc<Address>,
-        pub pool_package: Arc<Address>,
-        pub transaction_processor_package: Arc<Address>,
-        pub metadata_module_package: Arc<Address>,
-        pub royalty_module_package: Arc<Address>,
-        pub role_assignment_module_package: Arc<Address>,
-        pub genesis_helper_package: Arc<Address>,
-        pub faucet_package: Arc<Address>,
-    }
-}
-
-impl_new_from_network! {
-    #[derive(Clone, Debug, Record)]
-    pub struct ComponentAddresses {
-        pub consensus_manager: Arc<Address>,
-        pub genesis_helper: Arc<Address>,
-        pub faucet: Arc<Address>,
-    }
-}
-
-macro_rules! impl_new_from_network {
+macro_rules! define_known_addresses {
     (
-        $(#[$meta: meta])*
-        $vis: vis struct $ident: ident {
-            $(
-                $item_vis: vis $item_ident: ident: $item_type: ty
-            ),* $(,)?
-        }
+        $(
+            $ty: ident => [
+                $(
+                    $ident: ident
+                ),* $(,)?
+            ]
+        ),* $(,)?
     ) => {
         paste::paste! {
-            $(#[$meta])*
-            $vis struct $ident {
+            #[derive(Clone, Debug, Record)]
+            pub struct KnownAddresses {
                 $(
-                    $item_vis $item_ident: $item_type,
+                    pub $ty: [< $ty: camel >],
                 )*
             }
 
-            impl $ident {
+            impl KnownAddresses {
                 pub fn new_from_network(network_id: u8) -> Self {
                     Self {
                         $(
-                            $item_ident: Arc::new(Address( scrypto::prelude::[< $item_ident: upper >].into_node_id(), network_id )),
+                            $ty: [< $ty: camel >]::new_from_network(network_id),
                         )*
                     }
                 }
             }
+
+            $(
+                #[derive(Clone, Debug, Record)]
+                pub struct [< $ty: camel >] {
+                    $(
+                        pub $ident: $crate::prelude::Arc<$crate::prelude::Address>,
+                    )*
+                }
+
+                impl [< $ty: camel >] {
+                    pub fn new_from_network(network_id: u8) -> Self {
+                        Self {
+                            $(
+                                $ident: $crate::prelude::Arc::new(Address( scrypto::prelude::[< $ident: upper >].into_node_id(), network_id )),
+                            )*
+                        }
+                    }
+                }
+            )*
         }
     };
 }
-use impl_new_from_network;
+define_known_addresses! {
+    resource_addresses => [
+        xrd,
+        secp256k1_signature_virtual_badge,
+        ed25519_signature_virtual_badge,
+        package_of_direct_caller_virtual_badge,
+        global_caller_virtual_badge,
+        system_transaction_badge,
+        package_owner_badge,
+        validator_owner_badge,
+        account_owner_badge,
+        identity_owner_badge,
+    ],
+    package_addresses => [
+        package_package,
+        resource_package,
+        account_package,
+        identity_package,
+        consensus_manager_package,
+        access_controller_package,
+        pool_package,
+        transaction_processor_package,
+        metadata_module_package,
+        royalty_module_package,
+        role_assignment_module_package,
+        genesis_helper_package,
+        faucet_package,
+    ],
+    component_addresses => [
+        consensus_manager,
+        genesis_helper,
+        faucet,
+    ]
+}
