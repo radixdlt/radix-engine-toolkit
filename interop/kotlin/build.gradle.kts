f0f8214296f5daf30f9059fc358956ed4cbeb26c
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-10-11 12:56:27 -0300

    DO-1905 update the gradle file to publish to maven central

--- interop/kotlin/build.gradle.kts
+++ interop/kotlin/build.gradle.kts
@@ -22,22 +22,34 @@ java {
 
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
             groupId = "com.radixdlt"
             artifactId = "radix-engine-toolkit-kotlin"
             version = providers.gradleProperty("ret-version").getOrNull()
 
-            from(components["java"])
+            pom {
+                name.set("Kotlin Radix Engine Toolkit")
+                description.set("Kotlin Radix Engine Toolkit")
+                url.set("https://github.com/radixdlt/radix-engine-toolkit")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+            }
         }
     }
-
     repositories {
         maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/radixdlt/radix-engine-toolkit")
+            name = "Sonatype"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = project.findProperty("ossrhUsername") ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword") ?: System.getenv("OSSRH_PASSWORD")
             }
         }
     }
