94a71d641683cbb14ae9490e724c324f16baa8e4
Santiago Baldassin <santiago.baldassin@rdx.works>
2023-10-24 13:57:28 -0300

    temp adding the kotlin folder

---
+++ interop/kotlin/ret-kotlin/lib/build.gradle.kts
@@ -0,0 +1,63 @@
+import org.gradle.jvm.tasks.Jar
+
+plugins {
+    kotlin("jvm") version "1.8.21"
+    `java-library`
+    `maven-publish`
+    `signing`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+   implementation("net.java.dev.jna:jna:5.13.0")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.radixdlt"
+            artifactId = "radix-engine-toolkit-kotlin"
+            version = providers.gradleProperty("ret-version").getOrNull()
+
+            from(components["java"])
+            pom {
+                name.set("Radix Engine Toolkit")
+                description.set("Radix Engine Toolkit")
+                url.set("https://github.com/radixdlt/radix-engine-toolkit.git")
+            }
+        }
+    }
+
+    repositories {
+
+        maven {
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.findProperty("ossrhUsername") as String?
+                password = project.findProperty("ossrhPassword") as String?
+            }
+        }
+
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/radixdlt/radix-engine-toolkit")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["maven"])
+}
