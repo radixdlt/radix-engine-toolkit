e150a9793721bc6edfcc4a09ecdab6a1f16dddeb
Omar <OmarAbdulla7@hotmail.com>
2023-12-12 10:27:46 +0300

    Validator unstaking detector

--- crates/radix-engine-toolkit/src/transaction_types/traverser/types/mod.rs
+++ crates/radix-engine-toolkit/src/transaction_types/traverser/types/mod.rs
@@ -4,6 +4,7 @@ mod pool_contribution;
 mod pool_redemption;
 mod transfer;
 mod validator_stake;
+mod validator_unstake;
 
 pub use account_settings::*;
 pub use general::*;
@@ -11,3 +12,4 @@ pub use pool_contribution::*;
 pub use pool_redemption::*;
 pub use transfer::*;
 pub use validator_stake::*;
+pub use validator_unstake::*;
---
+++ crates/radix-engine-toolkit/src/transaction_types/traverser/types/validator_unstake.rs
@@ -0,0 +1,239 @@
+use std::ops::*;
+
+use scrypto::prelude::*;
+use transaction::prelude::*;
+
+use radix_engine_interface::blueprints::account::*;
+use radix_engine_interface::blueprints::consensus_manager::*;
+
+use crate::transaction_types::*;
+use crate::utils::*;
+
+pub struct TrackedUnstake {
+    /* Input */
+    pub liquid_stake_unit_address: ResourceAddress,
+    pub liquid_stake_unit_amount: Decimal,
+    /* Output */
+    pub claim_nft_address: ResourceAddress,
+    pub claim_nft_ids: IndexSet<NonFungibleLocalId>,
+}
+
+impl TrackedUnstake {
+    pub fn new(
+        liquid_stake_unit_address: ResourceAddress,
+        claim_nft_address: ResourceAddress,
+    ) -> Self {
+        Self {
+            liquid_stake_unit_address,
+            claim_nft_address,
+            claim_nft_ids: Default::default(),
+            liquid_stake_unit_amount: Default::default(),
+        }
+    }
+}
+
+pub struct ValidatorUnstakeDetector {
+    is_valid: bool,
+    /// The validators encountered in this manifest that were staked to.
+    validators: IndexSet<GlobalAddress>,
+    /// Tracks the staked resources. Maps a validator to the tracked stakes
+    /// data.
+    tracked_unstake: IndexMap<ComponentAddress, TrackedUnstake>,
+}
+
+impl ManifestSummaryCallback for ValidatorUnstakeDetector {
+    fn on_instruction(
+        &mut self,
+        instruction: &InstructionV1,
+        instruction_index: usize,
+    ) {
+        self.is_valid &= match instruction {
+            /* Maybe Permitted - Need more info */
+            InstructionV1::CallMethod {
+                address,
+                method_name,
+                ..
+            } => {
+                Self::construct_fn_rules(address).is_fn_permitted(&method_name)
+            }
+            /* Permitted */
+            InstructionV1::TakeFromWorktop { .. }
+            | InstructionV1::TakeNonFungiblesFromWorktop { .. }
+            | InstructionV1::TakeAllFromWorktop { .. }
+            | InstructionV1::AssertWorktopContainsAny { .. }
+            | InstructionV1::AssertWorktopContains { .. }
+            | InstructionV1::AssertWorktopContainsNonFungibles { .. } => true,
+            /* Not Permitted */
+            InstructionV1::BurnResource { .. }
+            | InstructionV1::CallRoyaltyMethod { .. }
+            | InstructionV1::CallMetadataMethod { .. }
+            | InstructionV1::CallRoleAssignmentMethod { .. }
+            | InstructionV1::CallDirectVaultMethod { .. }
+            | InstructionV1::AllocateGlobalAddress { .. }
+            | InstructionV1::ReturnToWorktop { .. }
+            | InstructionV1::PopFromAuthZone
+            | InstructionV1::PushToAuthZone { .. }
+            | InstructionV1::CreateProofFromAuthZoneOfAmount { .. }
+            | InstructionV1::CreateProofFromAuthZoneOfNonFungibles { .. }
+            | InstructionV1::CreateProofFromAuthZoneOfAll { .. }
+            | InstructionV1::DropAuthZoneProofs
+            | InstructionV1::DropAuthZoneRegularProofs
+            | InstructionV1::DropAuthZoneSignatureProofs
+            | InstructionV1::CreateProofFromBucketOfAmount { .. }
+            | InstructionV1::CreateProofFromBucketOfNonFungibles { .. }
+            | InstructionV1::CreateProofFromBucketOfAll { .. }
+            | InstructionV1::CloneProof { .. }
+            | InstructionV1::DropProof { .. }
+            | InstructionV1::DropNamedProofs
+            | InstructionV1::DropAllProofs
+            | InstructionV1::CallFunction { .. } => false,
+        }
+    }
+
+    fn on_global_entity_encounter(&mut self, address: GlobalAddress) {
+        if address
+            .as_node_id()
+            .entity_type()
+            .is_some_and(|entity_type| {
+                matches!(entity_type, EntityType::GlobalValidator)
+            })
+        {
+            self.validators.insert(address);
+        }
+    }
+}
+
+impl ExecutionSummaryCallback for ValidatorUnstakeDetector {
+    fn on_instruction(
+        &mut self,
+        instruction: &InstructionV1,
+        instruction_index: usize,
+        input_resources: Vec<&SourceResourceSpecifier>,
+        output_resources: Vec<&SourceResourceSpecifier>,
+    ) {
+        match instruction {
+            InstructionV1::CallMethod {
+                address: dynamic_address @ DynamicGlobalAddress::Static(address),
+                method_name,
+                ..
+            } if is_validator(dynamic_address)
+                && (method_name == VALIDATOR_UNSTAKE_IDENT) =>
+            {
+                let Some(SourceResourceSpecifier::Amount(
+                    stake_unit_resource_address,
+                    stake_unit_amount,
+                )) = input_resources.first()
+                else {
+                    // Can happen if an empty bucket of LSUs is provided as
+                    // input.
+                    return;
+                };
+
+                let Some(SourceResourceSpecifier::Ids(
+                    claim_nft_resource_address,
+                    claim_nft_ids,
+                )) = output_resources.first()
+                else {
+                    // TODO: Error? Panic? Is it ever possible that we get back
+                    // no claim NFTs? If not, then I would say we do an unwrap
+                    // here and force a crash if this ever happens.
+                    return;
+                };
+
+                let validator_component = ComponentAddress::try_from(*address)
+                    .expect("Must succeed!");
+
+                let entry = self
+                    .tracked_unstake
+                    .entry(validator_component)
+                    .or_insert(TrackedUnstake::new(
+                        *stake_unit_resource_address,
+                        *claim_nft_resource_address,
+                    ));
+                entry.liquid_stake_unit_amount += *stake_unit_amount.deref();
+                entry.claim_nft_ids.extend(claim_nft_ids.deref().clone())
+            }
+            _ => { /* No-op */ }
+        }
+    }
+}
+
+impl ValidatorUnstakeDetector {
+    pub fn is_valid(&self) -> bool {
+        self.is_valid
+    }
+
+    fn construct_fn_rules(address: &DynamicGlobalAddress) -> FnRules {
+        match address {
+            DynamicGlobalAddress::Named(..) => FnRules::all_disallowed(),
+            DynamicGlobalAddress::Static(address) => {
+                address
+                    .as_node_id()
+                    .entity_type()
+                    .map(|entity_type| {
+                        match entity_type {
+                        EntityType::GlobalAccount
+                        | EntityType::GlobalVirtualSecp256k1Account
+                        | EntityType::GlobalVirtualEd25519Account => FnRules {
+                            allowed: &[
+                                /* All withdraw methods */
+                                ACCOUNT_WITHDRAW_IDENT,
+                                /* All deposit methods */
+                                ACCOUNT_DEPOSIT_IDENT,
+                                ACCOUNT_DEPOSIT_BATCH_IDENT,
+                                ACCOUNT_TRY_DEPOSIT_OR_ABORT_IDENT,
+                                ACCOUNT_TRY_DEPOSIT_BATCH_OR_ABORT_IDENT,
+                            ],
+                            disallowed: &[],
+                            default: FnRule::Disallowed,
+                        },
+                        EntityType::GlobalValidator => FnRules {
+                            allowed: &[
+                                VALIDATOR_UNSTAKE_IDENT
+                            ],
+                            disallowed: &[],
+                            default: FnRule::Disallowed
+                        },
+                        /* Disallowed */
+                        EntityType::GlobalGenericComponent
+                        | EntityType::GlobalIdentity
+                        | EntityType::GlobalVirtualSecp256k1Identity
+                        | EntityType::GlobalVirtualEd25519Identity
+                        | EntityType::InternalGenericComponent
+                        | EntityType::GlobalPackage
+                        | EntityType::GlobalFungibleResourceManager
+                        | EntityType::GlobalNonFungibleResourceManager
+                        | EntityType::GlobalConsensusManager
+                        | EntityType::InternalFungibleVault
+                        | EntityType::InternalNonFungibleVault
+                        | EntityType::InternalKeyValueStore
+                        | EntityType::GlobalTransactionTracker
+                        | EntityType::GlobalAccessController
+                        | EntityType::GlobalOneResourcePool
+                        | EntityType::GlobalTwoResourcePool
+                        | EntityType::GlobalMultiResourcePool
+                         => FnRules::all_disallowed(),
+                    }
+                    })
+                    .unwrap_or(FnRules::all_disallowed())
+            }
+        }
+    }
+}
+
+fn is_pool(address: &DynamicGlobalAddress) -> bool {
+    match address {
+        DynamicGlobalAddress::Static(address) => address
+            .as_node_id()
+            .entity_type()
+            .is_some_and(|entity_type| {
+                matches!(
+                    entity_type,
+                    EntityType::GlobalOneResourcePool
+                        | EntityType::GlobalTwoResourcePool
+                        | EntityType::GlobalMultiResourcePool
+                )
+            }),
+        DynamicGlobalAddress::Named(_) => false,
+    }
+}
