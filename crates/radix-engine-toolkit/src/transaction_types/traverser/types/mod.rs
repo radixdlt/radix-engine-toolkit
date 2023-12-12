262164a1e03c74b74802050c47400b24160dce94
Omar <OmarAbdulla7@hotmail.com>
2023-12-12 10:16:46 +0300

    Add a validator stake detector

--- crates/radix-engine-toolkit/src/transaction_types/traverser/types/mod.rs
+++ crates/radix-engine-toolkit/src/transaction_types/traverser/types/mod.rs
@@ -3,9 +3,11 @@ mod general;
 mod pool_contribution;
 mod pool_redemption;
 mod transfer;
+mod validator_stake;
 
 pub use account_settings::*;
 pub use general::*;
 pub use pool_contribution::*;
 pub use pool_redemption::*;
 pub use transfer::*;
+pub use validator_stake::*;
---
+++ crates/radix-engine-toolkit/src/transaction_types/traverser/types/validator_stake.rs
@@ -0,0 +1,227 @@
+use std::ops::*;
+
+use scrypto::prelude::*;
+use transaction::prelude::*;
+
+use radix_engine_interface::blueprints::account::*;
+use radix_engine_interface::blueprints::consensus_manager::*;
+
+use crate::transaction_types::*;
+use crate::utils::*;
+
+pub struct TrackedStake {
+    pub liquid_stake_unit_address: ResourceAddress,
+    pub liquid_stake_unit_amount: Decimal,
+    pub xrd_amount: Decimal,
+}
+
+pub struct ValidatorStakeDetector {
+    is_valid: bool,
+    /// The validators encountered in this manifest that were staked to.
+    validators: IndexSet<GlobalAddress>,
+    /// Tracks the staked resources. Maps a validator to the tracked stakes
+    /// data
+    tracked_stake: IndexMap<ComponentAddress, TrackedStake>,
+}
+
+impl ManifestSummaryCallback for ValidatorStakeDetector {
+    fn on_instruction(
+        &mut self,
+        instruction: &InstructionV1,
+        instruction_index: usize,
+    ) {
+        self.is_valid &= match instruction {
+            /* Maybe Permitted - Need more info */
+            InstructionV1::CallMethod {
+                address,
+                method_name,
+                ..
+            } => {
+                Self::construct_fn_rules(address).is_fn_permitted(&method_name)
+            }
+            /* Permitted */
+            InstructionV1::TakeFromWorktop { .. }
+            | InstructionV1::TakeNonFungiblesFromWorktop { .. }
+            | InstructionV1::TakeAllFromWorktop { .. }
+            | InstructionV1::AssertWorktopContainsAny { .. }
+            | InstructionV1::AssertWorktopContains { .. }
+            | InstructionV1::AssertWorktopContainsNonFungibles { .. } => true,
+            /* Not Permitted */
+            InstructionV1::BurnResource { .. }
+            | InstructionV1::CallRoyaltyMethod { .. }
+            | InstructionV1::CallMetadataMethod { .. }
+            | InstructionV1::CallRoleAssignmentMethod { .. }
+            | InstructionV1::CallDirectVaultMethod { .. }
+            | InstructionV1::AllocateGlobalAddress { .. }
+            | InstructionV1::ReturnToWorktop { .. }
+            | InstructionV1::PopFromAuthZone
+            | InstructionV1::PushToAuthZone { .. }
+            | InstructionV1::CreateProofFromAuthZoneOfAmount { .. }
+            | InstructionV1::CreateProofFromAuthZoneOfNonFungibles { .. }
+            | InstructionV1::CreateProofFromAuthZoneOfAll { .. }
+            | InstructionV1::DropAuthZoneProofs
+            | InstructionV1::DropAuthZoneRegularProofs
+            | InstructionV1::DropAuthZoneSignatureProofs
+            | InstructionV1::CreateProofFromBucketOfAmount { .. }
+            | InstructionV1::CreateProofFromBucketOfNonFungibles { .. }
+            | InstructionV1::CreateProofFromBucketOfAll { .. }
+            | InstructionV1::CloneProof { .. }
+            | InstructionV1::DropProof { .. }
+            | InstructionV1::DropNamedProofs
+            | InstructionV1::DropAllProofs
+            | InstructionV1::CallFunction { .. } => false,
+        }
+    }
+
+    fn on_global_entity_encounter(&mut self, address: GlobalAddress) {
+        if address
+            .as_node_id()
+            .entity_type()
+            .is_some_and(|entity_type| {
+                matches!(entity_type, EntityType::GlobalValidator)
+            })
+        {
+            self.validators.insert(address);
+        }
+    }
+}
+
+impl ExecutionSummaryCallback for ValidatorStakeDetector {
+    fn on_instruction(
+        &mut self,
+        instruction: &InstructionV1,
+        instruction_index: usize,
+        input_resources: Vec<&SourceResourceSpecifier>,
+        output_resources: Vec<&SourceResourceSpecifier>,
+    ) {
+        match instruction {
+            InstructionV1::CallMethod {
+                address: dynamic_address @ DynamicGlobalAddress::Static(address),
+                method_name,
+                ..
+            } if is_validator(dynamic_address)
+                && (method_name == VALIDATOR_STAKE_IDENT) =>
+            {
+                // TODO: Filter + Pattern matching is perhaps redundant?
+                let Some(SourceResourceSpecifier::Amount(XRD, xrd_amount)) =
+                    input_resources
+                        .iter()
+                        .filter(|specifier| specifier.resource_address() == XRD)
+                        .next()
+                else {
+                    // Can happen if an empty bucket of XRD is provided as
+                    // input.
+                    return;
+                };
+
+                let Some(SourceResourceSpecifier::Amount(
+                    stake_units_resource_address,
+                    stake_units_amount,
+                )) = output_resources.first()
+                else {
+                    // TODO: Error? Panic? This is especially concerning because
+                    // we can never get no stake units back when there is some
+                    // XRD input. So, is this even a real case? Maybe just
+                    // unwrap since it's impossible to get here?
+                    return;
+                };
+
+                let validator_component = ComponentAddress::try_from(*address)
+                    .expect("Must succeed!");
+
+                let entry = self
+                    .tracked_stake
+                    .entry(validator_component)
+                    .or_insert(TrackedStake {
+                        liquid_stake_unit_address:
+                            *stake_units_resource_address,
+                        liquid_stake_unit_amount: Decimal::ZERO,
+                        xrd_amount: Decimal::ZERO,
+                    });
+                entry.xrd_amount += *xrd_amount.deref();
+                entry.liquid_stake_unit_amount += *stake_units_amount.deref();
+            }
+            _ => { /* No-op */ }
+        }
+    }
+}
+
+impl ValidatorStakeDetector {
+    pub fn is_valid(&self) -> bool {
+        self.is_valid
+    }
+
+    fn construct_fn_rules(address: &DynamicGlobalAddress) -> FnRules {
+        match address {
+            DynamicGlobalAddress::Named(..) => FnRules::all_disallowed(),
+            DynamicGlobalAddress::Static(address) => {
+                address
+                    .as_node_id()
+                    .entity_type()
+                    .map(|entity_type| {
+                        match entity_type {
+                        EntityType::GlobalAccount
+                        | EntityType::GlobalVirtualSecp256k1Account
+                        | EntityType::GlobalVirtualEd25519Account => FnRules {
+                            allowed: &[
+                                /* All withdraw methods */
+                                ACCOUNT_WITHDRAW_IDENT,
+                                /* All deposit methods */
+                                ACCOUNT_DEPOSIT_IDENT,
+                                ACCOUNT_DEPOSIT_BATCH_IDENT,
+                                ACCOUNT_TRY_DEPOSIT_OR_ABORT_IDENT,
+                                ACCOUNT_TRY_DEPOSIT_BATCH_OR_ABORT_IDENT,
+                            ],
+                            disallowed: &[],
+                            default: FnRule::Disallowed,
+                        },
+                        EntityType::GlobalValidator => FnRules {
+                            allowed: &[
+                                VALIDATOR_STAKE_IDENT
+                            ],
+                            disallowed: &[],
+                            default: FnRule::Disallowed
+                        },
+                        /* Disallowed */
+                        EntityType::GlobalGenericComponent
+                        | EntityType::GlobalIdentity
+                        | EntityType::GlobalVirtualSecp256k1Identity
+                        | EntityType::GlobalVirtualEd25519Identity
+                        | EntityType::InternalGenericComponent
+                        | EntityType::GlobalPackage
+                        | EntityType::GlobalFungibleResourceManager
+                        | EntityType::GlobalNonFungibleResourceManager
+                        | EntityType::GlobalConsensusManager
+                        | EntityType::InternalFungibleVault
+                        | EntityType::InternalNonFungibleVault
+                        | EntityType::InternalKeyValueStore
+                        | EntityType::GlobalTransactionTracker
+                        | EntityType::GlobalAccessController
+                        | EntityType::GlobalOneResourcePool
+                        | EntityType::GlobalTwoResourcePool
+                        | EntityType::GlobalMultiResourcePool
+                         => FnRules::all_disallowed(),
+                    }
+                    })
+                    .unwrap_or(FnRules::all_disallowed())
+            }
+        }
+    }
+}
+
+fn is_pool(address: &DynamicGlobalAddress) -> bool {
+    match address {
+        DynamicGlobalAddress::Static(address) => address
+            .as_node_id()
+            .entity_type()
+            .is_some_and(|entity_type| {
+                matches!(
+                    entity_type,
+                    EntityType::GlobalOneResourcePool
+                        | EntityType::GlobalTwoResourcePool
+                        | EntityType::GlobalMultiResourcePool
+                )
+            }),
+        DynamicGlobalAddress::Named(_) => false,
+    }
+}
