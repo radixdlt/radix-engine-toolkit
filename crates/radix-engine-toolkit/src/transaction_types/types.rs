52d525f2acb203e33e9cc112e6ad433288e185fd
Michał Strug <michal.strug@rdx.works>
2024-01-19 12:23:05 +0100

    Fixed formatting

--- crates/radix-engine-toolkit/src/transaction_types/types.rs
+++ crates/radix-engine-toolkit/src/transaction_types/types.rs
@@ -91,7 +91,7 @@ pub struct ExecutionSummary {
     pub detailed_classification: Vec<DetailedManifestClass>,
     /// List of newly created Non-Fungibles during this transaction.
     pub newly_created_non_fungibles: HashSet<NonFungibleGlobalId>,
-    /// Content of the worktop and information if it is trusted at executed 
+    /// Content of the worktop and information if it is trusted at executed
     /// manifest instruction (instruction is the index of the vector)
     pub trusted_worktop_content: Vec<(WorktopContent, bool)>,
 }
