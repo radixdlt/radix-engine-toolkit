39d2522e99f8f6d8c4922cf49fc0c340a223d153
Michał Strug <michal.strug@rdx.works>
2024-04-30 15:19:01 +0200

    Updated version to v2.1.0-dev1

--- scripts/version_update.sh
+++ scripts/version_update.sh
@@ -6,9 +6,9 @@
 # Requires cargo-edit utility, to install run command: cargo install cargo-edit
 #
 
-old_version="1.0.10"
-new_version="2.0.0"
+old_version="2.0.0"
+new_version="2.1.0-dev1"
 
 cd ..
 cargo set-version $new_version
-sed -i "" -e "s/$old_version/$new_version/g" crates/radix-engine-toolkit-uniffi/tests/bindings/example.kts crates/radix-engine-toolkit-uniffi/tests/bindings/example.py crates/radix-engine-toolkit-uniffi/tests/bindings/example.swift crates/radix-engine-toolkit-uniffi/tests/bindings/example_test.go
+sed -i "" -e "s/\"$old_version\"/\"$new_version\"/g" crates/radix-engine-toolkit-uniffi/tests/bindings/example.kts crates/radix-engine-toolkit-uniffi/tests/bindings/example.py crates/radix-engine-toolkit-uniffi/tests/bindings/example.swift crates/radix-engine-toolkit-uniffi/tests/bindings/example_test.go
