d327d250bb9fb226027cb192bd32967fe7468c2a
Omar <OmarAbdulla7@hotmail.com>
2024-05-27 10:51:09 +0300

    Update typeshare and fix wasm compilation

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -1,9 +1,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cargo +nightly build \
+cargo +nightly-2023-11-24 build \
     -Z build-std=std,panic_abort \
     -Z build-std-features=panic_immediate_abort \
-    --manifest-path="$SCRIPT_DIR/../crates/radix-engine-toolkit/Cargo.toml" \
+    --manifest-path="$SCRIPT_DIR/../Cargo.toml" \
+    --package radix-engine-toolkit-json \
     --target wasm32-unknown-unknown \
     --release
 npx wasm-opt@1.4 -Oz -g \
@@ -11,5 +12,5 @@ npx wasm-opt@1.4 -Oz -g \
     --strip-debug \
     --strip-producers \
     -o \
-    "$SCRIPT_DIR/../target/wasm32-unknown-unknown/release/radix_engine_toolkit.wasm" \
-    "$SCRIPT_DIR/../target/wasm32-unknown-unknown/release/radix_engine_toolkit.wasm"
+    "$SCRIPT_DIR/../target/wasm32-unknown-unknown/release/radix_engine_toolkit_json.wasm" \
+    "$SCRIPT_DIR/../target/wasm32-unknown-unknown/release/radix_engine_toolkit_json.wasm"
--- scripts/typeshare.sh
+++ scripts/typeshare.sh
@@ -2,5 +2,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p $SCRIPT_DIR/../crates/generator/output/typeshare
 
-typeshare $SCRIPT_DIR --lang=typescript --output-file=$SCRIPT_DIR/../crates/generator/output/typeshare/generated.ts
+typeshare $SCRIPT_DIR/../crates/radix-engine-toolkit-json --lang=typescript --output-file=$SCRIPT_DIR/../crates/generator/output/typeshare/generated.ts
 prettier --write $SCRIPT_DIR/../crates/generator/output/typeshare/*.ts
