28255cd02fc7f277c5b3f8a012b19d75ca0d03d7
Sergiu Puhalschi <sergiu@radixpublishing.com>
2025-11-26 16:36:51 +0200

    Add debug logs

--- interop/kotlin/build.gradle.kts
+++ interop/kotlin/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.jvm.tasks.Jar
+println(">>> USING ROOT: ${project.projectDir}")
 
 plugins {
     kotlin("jvm") version "1.8.21"
@@ -81,6 +82,9 @@ publishing {
         maven {
             name = "mavenCentral"
             url = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+
+            println("Configured mavenCentral repo for ${project.path} with url=$url")
+
             credentials {
                 username = project.findProperty("ossrhUsername") as String?
                 password = project.findProperty("ossrhPassword") as String?
